648631c5d673526e937f09d8ddccf14970bbdc67
Ademir <ademir.m.junior@gmail.com>
2021-09-05 17:42:41 -0300

    Develop basic ui

--- src/app/room-list/room-list.component.ts
+++ src/app/room-list/room-list.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { Room } from '../room';
+import { RoomService } from '../room.service';
 
 @Component({
   selector: 'app-room-list',
@@ -6,10 +10,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./room-list.component.css']
 })
 export class RoomListComponent implements OnInit {
-
-  constructor() { }
+  rooms!: Observable<Room[]>;
+  
+  constructor(private roomService: RoomService,
+              private router: Router) { }
 
   ngOnInit(): void {
+    this.reloadData();
+  }
+
+  reloadData() {
+    this.rooms = this.roomService.getRoomList();
+  }
+
+  deleteRoom(id: number) {
+    this.roomService.deleteRoom(id)
+    .subscribe(
+      data => {
+        console.log(data)
+        this.reloadData();
+      },
+      error => console.log(error)
+    );
+  }
+
+  roomDetails(id: number) {
+    this.router.navigate(['details', id]);
+  }
+
+  updateRoom(id: number) {
+    this.router.navigate(['update', id])
   }
 
 }
